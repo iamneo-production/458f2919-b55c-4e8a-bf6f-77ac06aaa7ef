1dfb74c913e34c260fbc6fb8f4c927e05c87c1a7
Megaranjani <72685083+Megaranjani@users.noreply.github.com>
2022-04-05 10:14:58 +0530

    Update Orders.js

--- reactapp/src/components/Orders.js
+++ reactapp/src/components/Orders.js
@@ -1,9 +1,11 @@
 import Navbar1 from "./Navbar/Navbar1";
+import Table from "./Table";
 
 function Orders(){
     return(
         <div className='order'>
             <Navbar1/>
+            <Table/>
 
             
         </div>
